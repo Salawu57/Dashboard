cebb413a165ffc10c6dfb5553d16813b81f71549
Salawu57 <fisco57@yahoo.com>
2021-09-14 08:47:12 +0100

    add secure to boot

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        If (env('APP_ENV') !== 'local') {
+            $this->app['request']->server->set('HTTPS', true);
+        }
         //
     }
 }
