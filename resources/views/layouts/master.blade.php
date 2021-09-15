114ba67a0e564a87c13ef63de2c7cf2e665f74c2
Salawu57 <fisco57@yahoo.com>
2021-09-15 09:19:44 +0100

    remove some user options

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -105,8 +105,8 @@
         <li class="treeview"> <a href="{{ route('home') }}"> <i class="icon-people"></i> <span>Users</span> <span class="pull-right-container"> <i class="fa fa-angle-left pull-right"></i> </span> </a>
         <ul class="treeview-menu">
 
-            <li><a href="{{route('users.index')}}"><i class="fa fa-angle-right"></i>All Users</a></li>
-            <li><a href="{{route('users.create')}}"><i class="fa fa-angle-right"></i>Create New User</a></li>
+            <!-- <li><a href="{{route('users.index')}}"><i class="fa fa-angle-right"></i>All Users</a></li>
+            <li><a href="{{route('users.create')}}"><i class="fa fa-angle-right"></i>Create New User</a></li> -->
 
             <li><a href="{{route('users.profile')}}"><i class="fa fa-angle-right"></i>Profile</a></li>
 
